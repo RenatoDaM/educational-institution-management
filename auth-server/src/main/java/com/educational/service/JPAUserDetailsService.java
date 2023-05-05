6eb11270df665831acefdfeee56ef7b5c0af7a88
Renato <renaato.d.m@gmail.com>
2023-05-05 16:19:02 -0300

    implementando quem vai oferecer os recursos após aceitar a autenticação. Provavelmente irei fundir com a application o resource-server após entender bem a lógica.

--- auth-server/src/main/java/com/educational/service/JPAUserDetailsService.java
+++ auth-server/src/main/java/com/educational/service/JPAUserDetailsService.java
@@ -20,6 +20,28 @@ public class JPAUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+
+    /*
+    O GrantedAuthority é uma interface que define a autoridade concedida a um usuário.
+    É usada pelo Spring Security para realizar a autorização com base nas
+    permissões atribuídas a cada usuário. O SimpleGrantedAuthority é uma
+    implementação básica de GrantedAuthority, que apenas armazena uma String representando a
+    autoridade do usuário.
+
+    Neste código, é criada uma instância de SimpleGrantedAuthority com o nome da autoridade do
+    usuário, que é derivado do tipo do usuário retornado do userRepository. Em seguida,
+    é criada uma instância de UserDetails com as informações do usuário (nome e senha)
+    e a lista de autoridades concedidas (uma lista que contém apenas a instância de
+    SimpleGrantedAuthority criada anteriormente).
+
+    A razão pela qual é utilizada uma lista List.of para as autoridades é porque um usuário pode
+    ter mais de uma autoridade associada a ele. Mesmo que neste caso a lista contenha
+    apenas uma autoridade, o uso da lista permite que o método seja flexível e possa lidar com
+    cenários em que um usuário possa ter várias autoridades atribuídas a ele. Além disso,
+    usar List.of garante que a lista seja imutável e evita a possibilidade de modificação não intencional
+    na lista após sua criação.
+    */
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         final var user = userRepository.findByEmail(email);
